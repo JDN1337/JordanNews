76155da0d7f80e6dc6b61e6144bf4b378f95c387
Jordan Lepretre <JordanLepretre.JL@gmail.com>
2018-03-02 09:56:52 +0100

    Initial Commit

---
+++ JordanNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JordanNews
+//
+//  Created by Jordan Lepretre on 02/03/2018.
+//  Copyright © 2018 JDN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JordanNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JordanNews
+//
+//  Created by Jordan Lepretre on 02/03/2018.
+//  Copyright © 2018 JDN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
